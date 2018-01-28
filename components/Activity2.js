db3af5af8d08352299c37fb525f4eb49d9af2e51
gladydennyson <gladydennyson184@gmail.com>
2018-01-28 20:25:58 +0530

    Activity2

--- components/Activity2.js
+++ components/Activity2.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import * as Progress from 'react-native-progress';
+
+import { Card } from 'react-native-elements'
 
 var question = 'How are you ?';
 var current_ans = 'What would be your reply?';
@@ -25,14 +28,22 @@ export default class App extends Component {
     
     return (
       <View style={styles.container}>
-          <View style={styles.questBox}>
-            <Text style={{fontSize:20}}>{question}</Text>
+      <View style={{margin:20}}>
+          <Progress.Bar progress={0.6} height={8} unfilledColor={'rgba(154,154,154,1)'}width={320} borderWidth={0} borderRadius={0} />
           </View>
+          <Text style={styles.titleQuestion}>
+          Select the right option
+          </Text>
           
+            <View style={styles.questBox}>
+               
+            <Text style={{fontSize:20}}>{question}</Text>
+            </View>
+
+
           <View style={styles.ansBox}>
               <Text style={{fontSize:20, fontWeight:'bold'}}>{this.state.current_ans}</Text>
           </View>
-          
           <View style={styles.opsBox}>
             
               <TouchableOpacity onPress={() => this._handleButtonPress(ops[0])}>
@@ -60,79 +71,100 @@ export default class App extends Component {
               </TouchableOpacity>
                           
           </View>
-        
+
           <View style={styles.subBox}>
-            <TouchableOpacity>
-              <View style={styles.subButton}>
-                <Text style={{fontSize:20, fontWeight:'bold', color:'#ffffff'}}>SUBMIT</Text>
-              </View>
-          </TouchableOpacity>
-          </View>
-        
-      </View>
+                    <TouchableOpacity onPress={() => this._handleSubmitPress()}>
+                      <View style={styles.button1}>
+                        <Text style={{fontSize:20, color:'#ffffff'}}>SUBMIT</Text>
+                      </View>
+                    </TouchableOpacity>
+            </View>
+        </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    paddingTop: 10,
-    backgroundColor: '#34495e',
+    backgroundColor: '#e5e5e5',
   },
-  
+  timer: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    height: 20,
+    alignSelf: 'flex-start'
+
+  },
   questBox: {
-    flex: 1,
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: '#9FA8DA',
+    marginLeft:20,
+    backgroundColor: '#e5e5e5',
   },
   
   ansBox: {
-    flex: 1,
+    flex: 2,
     alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#7986CB',
+    margin:20,
+    backgroundColor: '#e5e5e5',
+  },
+  
+  ansLine: {
+    flexDirection:'row', 
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   },
   
   opsBox: {
-    flex: 3,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: '#5C6BC0',
+    flex:6,
+    borderRadius:5,
+    marginLeft:15
   },
   
   subBox: {
-    flex: 1,
+    flex: 2,
     alignItems: 'center',
     alignSelf: 'stretch',
     justifyContent: 'center',
-    backgroundColor: '#3F51B5',
+    backgroundColor: '#e5e5e5',
+  },
+  
+  buttonLine: {
+    flexDirection:'row', 
+    alignSelf: 'stretch',
+    justifyContent: 'center',
   },
   
   button: {
+    width: 250,
+    margin:5,
     alignItems: 'center',
-    backgroundColor: '#3F51B5',
-    alignSelf: 'stretch',
-    borderWidth: 1,
-    borderColor: '#5C6BC0',
-    marginRight: 10,
-    marginLeft: 10,
-    borderRadius:20
+    backgroundColor: 'rgba(9, 22, 200, 0.6)',
+    borderRadius: 10,
+    height:40,
+    justifyContent:'center'
   },
-  
-  subButton: {
+    
+   button1: {
+    margin: 25,
+    width: 120,
     alignItems: 'center',
-    backgroundColor: '#3F51B5',
-    alignSelf: 'stretch',
+    backgroundColor: '#1c3370',
+    borderRadius:5
   },
   
   buttonText: {
     padding: 20,
     color: 'white',
     fontSize: 20,
-  }
+    fontWeight: 'bold',
+   
+  },
+ titleQuestion:{
+     justifyContent:'center',
+     fontSize:22,
+     fontWeight:'bold',
+     padding:10,
+     marginLeft:9,
+     color:'#000000'
+ }
 });
+
